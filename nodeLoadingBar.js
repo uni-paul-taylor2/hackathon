e886f7cf7aa9922c2524a16670b5c7c900fb91d4
uni-paul-taylor2 <123190211+uni-paul-taylor2@users.noreply.github.com>
2023-03-26 13:44:40 -0400

    Update nodeLoadingBar.js

--- nodeLoadingBar.js
+++ nodeLoadingBar.js
@@ -1,6 +1,10 @@
 async function showLoadingBar(currentProgress, totalProgress, word = "complete") {
-  let text = `[${(2 ** 20).toString(2).split('').map((_, i) => i / 20 <= currentProgress / totalProgress ? "|" : " ").join('')
-    }] - ${(100 * currentProgress / totalProgress).toFixed(2)}% ${word}`
+  let yellow='\x1b[1m\x1b[33m', cyan='\x1b[1m\x1b[36m', magenta='\x1b[1m\x1b[35m', reset='\x1b[0m'
+  
+  let text = yellow+`[${ cyan+
+    (2 ** 20).toString(2).split('').map((_, i) => i / 20 <= currentProgress / totalProgress ? "|" : " ").join('')
+  +yellow }]${magenta} - ${(100 * currentProgress / totalProgress).toFixed(2)}% ${word+reset}`
+  
   await new Promise(next => process.stdout.clearLine(0, next))
   await new Promise(next => process.stdout.cursorTo(0, next))
   process.stdout.write(text)
