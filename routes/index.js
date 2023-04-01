3c3cac06f814aa455fdeda794eb370e82b86ec7b
firepenguindisopanda <nicosmith.smith3@gmail.com>
2023-03-31 21:35:10 -0400

    Updated: the name of path for import controller

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/file.controller');
+const controller = require('../controller/file.controller');
 
 let routes = (app) => {
     router.post("/upload", controller.upload);
