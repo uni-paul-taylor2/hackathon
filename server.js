563a9df2159e647ed8354204b06c2c0787c8e7f5
firepenguindisopanda <nicosmith.smith3@gmail.com>
2023-03-31 23:27:04 -0400

    Updated: setup of the assets folder and uploads sub directory

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const cors = require('cors');
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 
 global.__basedir = __dirname;
@@ -13,6 +15,16 @@ app.use(cors(corsOptions));
 const initRoutes = require("./routes");
 
 app.use(express.urlencoded({ extended: true }));
+
+// Create assets and uploads directories if they don't exist
+const assetsDir = path.join(__dirname, 'assets');
+if (!fs.existsSync(assetsDir)) {
+  fs.mkdirSync(assetsDir);
+}
+const uploadsDir = path.join(assetsDir, 'uploads');
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir);
+}
 
 initRoutes(app);
 
